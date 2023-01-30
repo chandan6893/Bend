27cd0d4004b1b430119a3d4fc0b4dc0b5dc6faef
Chandan Kumar <krc6893@gmail.com>
2023-01-30 14:46:42 +0530

    Bend

--- index.js
+++ index.js
@@ -37,37 +37,91 @@
 // });
 // server.listen(3000);
 
-const http = require("http");
-const server = http.createServer((req, res) => {
-  // console.log(req.url);==> '/'
-  res.setHeader("Content-Type", "text/html");
-  if (req.url === "/") {
-    res.write("<html>");
-    res.write("<head><title>AirCampus</title></head>");
+// const http = require("http");
+// const server = http.createServer((req, res) => {
+//   // console.log(req.url);==> '/'
+//   res.setHeader("Content-Type", "text/html");
+//   if (req.url === "/") {
+//     res.write("<html>");
+//     res.write("<head><title>AirCampus</title></head>");
+//     res.write(
+//       `<body>
+//               <form action="/message" metod="POST">
+//               <input type="text" name="mang"> 
+//               <button type="submit">Send</button> 
+//               </form>
+//             </body>`
+//     );
+//     res.write("</html>");
+//     res.end();
+//   } else if (req.url === "/message") {
+//     const body = [];
+//     req.on("data", (chunk) => {
+//       // {
+//       console.log(chunk);
+//       body.push(chunk);
+//       // }
+//     });
+
+//     req.on("end", () => {
+//       const parsedBody = Buffer.concat(body).toString();
+//       console.log(parsedBody);
+//     });
+//     console.log(req.body);
+//   }
+// });
+// server.listen(3000);
+
+
+
+const http = require('http');
+const fs = require('fs');
+
+const handler = (req,res)=>{
+  console.log(req.url);
+  res.setHeader('Content-Type','text/html');
+  let url = req.url;
+  let method= req.method;
+  if(url==='/'){
+    res.write('<html>');
+    res.write('<head><title>AirCampus</title></head>');
     res.write(
       `<body>
-              <form action="/message" metod="POST">
-              <input type="text" name="mang"> 
-              <button type="submit">Send</button> 
-              </form>
-            </body>`
+          <form action="/message" methyod="Post" >
+            <input type="text" name="mango" >
+            <button type="submit" >SEND</button>
+          </form>
+      </body>`
     );
-    res.write("</html>");
+    res.write('</html>');
     res.end();
-  } else if (req.url === "/message") {
-    const body = [];
-    req.on("data", (chunk) => {
-      // {
-      console.log(chunk);
-      body.push(chunk);
-      // }
+  } else if(url=== '/message'  && method === 'POST'){
+    const body=[];
+    req.on('data',(chunk)=> {
+      {
+        console.log(chunk);
+        body.push(chunk);
+      }
     });
 
-    req.on("end", () => {
+    req.on('end',() => {
       const parsedBody = Buffer.concat(body).toString();
       console.log(parsedBody);
     });
     console.log(req.body);
+    fs.writeFileSync('message.txt', 'Hello');
+    // res.statusCide = 302;/
+    // res.setHeader('Location','/');
+
+    // or,
+
+    res.writeHead(302,{
+      Location: 'https://www.google.com',
+    });
+    
+    res.end();
   }
-});
+};
+
+const server = http.createServer(handler);
 server.listen(3000);
