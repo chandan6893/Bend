cbd8abdb8ad2f7c9c53dce91d651506b551a5854
Chandan Kumar <krc6893@gmail.com>
2023-01-31 18:09:04 +0530

    express

--- index.js
+++ index.js
@@ -1,2 +1,84 @@
+// const express = require("express");
+// // eventHandler is given to us by expressJs
+// const app = express();
+// const http = require("http");
+
+// app.use((req,res,next)=>{
+//     console.log("firstMiddleware");
+//     next();
+// });
+
+// app.use((req,res,next)=>{
+//  console.log("secondMiddleware")
+// });
+// const server= http.createServer(app);
+// server.listen(3000);
+// console.log(app)
+
+
+
+// const express = require("express");
+// // eventHandler is given to us by expressJs
+// const app = express();
+// // const http = require("http");
+
+// app.use((req, res, next) => {
+//   console.log("firstMiddleware");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("secondMiddleware");
+// });
+// // const server = http.createServer(app);
+// // server.listen(3000);
+// app.listen(3000);
+
+
+
+// const express = require("express");
+// // eventHandler is given to us by expressJs
+// const app = express();
+// app.use((req, res, next) => {
+//     res.send("Hello Response")
+//   console.log("firstMiddleware");
+//   next();
+// });
+// app.use((req, res, next) => {
+//   console.log("secondMiddleware");
+// });
+// app.listen(3000);
+
+
+// const express = require("express");
+// // eventHandler is given to us by expressJs
+// const app = express();
+// app.use((req, res, next) => {
+//     let isLoggedIn = true;
+//     // let isLoggedIn = false;
+//     if(isLoggedIn){
+//          console.log("Validating User Login");
+//           next();
+//     }else{
+//          res.send("Invalid Credentials");
+//     }
+// }); 
+// app.use((req, res, next) => {
+//   res.send("Welcome To FaceBook");
+// });
+// app.listen(3000);
+
+
+
 const express = require("express");
 const app = express();
+
+app.use("/home", (req, res, next) => {
+  res.send("Home --> Welcome to facebook");
+});
+
+app.use('/',(req,res,next)=>{
+    res.send("Nothing")
+});
+
+app.listen(3000);
