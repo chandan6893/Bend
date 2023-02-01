d19d08f32591ddc611f5de8eea6566287a960487
Chandan Kumar <krc6893@gmail.com>
2023-02-01 19:46:47 +0530

    router

---
+++ routes/product.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use('/products',(req,res)=>{
+    res.send('Some products page');
+});
+
+router.use('/add-product',(req,res,next)=>{
+    res.send(
+      "<form action='/product' method='POST'> <input type='text' name='title'> <button type='submit'>Send</button></form>"
+    );
+});
+
+router.use('/product',(req,res)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports=router;
