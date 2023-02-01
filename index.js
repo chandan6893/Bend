14c1311c3f60306759e902d3542ba5fb0993cac8
Chandan Kumar <krc6893@gmail.com>
2023-02-01 19:27:29 +0530

    express

--- index.js
+++ index.js
@@ -85,21 +85,48 @@
 // app.listen(3000);
 
 
+// const express = require("express");
+// const app = express();
+
+// app.use("/home", (req, res, next) => {
+//   res.send(`<form action="/message" methyod="Post" >
+//              <input type="text" name="mango" >
+//              <button type="submit" >SEND</button>
+//             </form>`);
+// });
+
+// app.use("/message", (req, res, next) => {
+//   res.redirect("/home");
+// });
+
+// app.use("/",(req,res,next)=>{
+//     console.log("hello Guys")
+// })
+// app.listen(3000);
+
+
+
+
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
-app.use("/home", (req, res, next) => {
-  res.send(`<form action="/message" methyod="Post" >
-             <input type="text" name="mango" >
-             <button type="submit" >SEND</button>
-            </form>`);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'> <input type='text' name='title'> <button type='submit'>Send</button></form>"
+  );
 });
 
-app.use("/message", (req, res, next) => {
-  res.redirect("/home");
+app.use("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-app.use("/",(req,res,next)=>{
-    console.log("hello Guys")
-})
+app.use("/", (req, res) => {
+  res.send("home");
+});
+
 app.listen(3000);
