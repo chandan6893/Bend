8cbfea658501a6f9a3ef0e838bab48497df4930f
Chandan Kumar <krc6893@gmail.com>
2023-09-23 13:20:39 +0530

    mongoose,model,Schema,update records,deleteInDatabase_mongoose

---
+++ udeleteInDB_mongoose.js
@@ -0,0 +1,21 @@
+const mongoose=require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
+
+const productSchema=new mongoose.Schema(
+    {
+        name:String,
+        price:Number,
+        brnd:String,
+        category:String
+    }
+);
+
+const deleteInDB=async ()=>{
+    const Product=mongoose.model("products",productSchema);
+
+    let data=await Product.deleteOne(
+        {name:"Note Pro"}
+    );
+    console.log(data);
+}
+deleteInDB();
