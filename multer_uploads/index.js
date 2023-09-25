d4bdf16454263a7207ba21936c7a7ee89989350c
Chandan Kumar <krc6893@gmail.com>
2023-09-25 13:17:18 +0530

    upload image using multer

---
+++ multer_uploads/index.js
@@ -0,0 +1,21 @@
+// multer
+const express=require('express');
+const multer=require('multer');
+
+const app=express();
+
+const upload=multer({
+    storage:multer.diskStorage({
+        destination:function(req,file,cb){
+            cb(null, "../multer_Uploded_Images");
+        },
+        filename:function(req,file,cb){
+            cb(null,file.fieldname+"-"+Date.now()+".jpg")
+        }
+    })
+}).single("user_file");
+
+app.post('/upload',upload,(req,res)=>{
+    res.send("UPLODED")
+});
+app.listen(5000);
